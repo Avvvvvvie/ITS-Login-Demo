6c8335c4e95929a796dd58458c751bf9b9c41a09
Annie Hemprich <hemprann@students.zhaw.ch>
2025-10-27 19:43:12 +0100

    remove old signout snippet

--- public/index.js
+++ public/index.js
@@ -90,12 +90,6 @@ async function login(username, password) {
   }
 }
 
-function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
 
 window.onSignIn = async (googleUser) => {
     try {
